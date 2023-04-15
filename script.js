1c717613a9b29206e8419f56fc6bac7dd1986427
yuripadua <yuripadua@gmail.com>
2023-04-15 07:57:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-fff;
+let result = document.getElementById("result");
+
+function calculate(value) {
+  result.value += value;
+}
+
+function getResult() {
+  result.value = eval(result.value);
+}
+
+function clearResult() {
+  result.value = "";
+}
